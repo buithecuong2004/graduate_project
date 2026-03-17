879138b3e7c1a708b52102d0e98baa6b7ab8a520
Bullet2004 <buithecuong2004@gmail.com>
2026-03-17 23:34:48 +0700

    feat: create list post

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets'
+import { assets, dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
+import PostCard from '../components/PostCard'
 
 const Feed = () => {
 
@@ -22,13 +23,18 @@ const Feed = () => {
        <div >
         <StoriesBar/>
         <div className='p-4 space-y-6'>
-          List of post
+          {feeds.map((post)=>(
+            <PostCard key={post._id} post={post}/>
+          ))}
         </div>
        </div>
 
-       <div>
-        <div>
-          <h1>Sponsored</h1>
+       <div className='max-xl:hidden sticky top-0'>
+        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+          <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+          <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
+          <p className='text-slate-600'>Email marketing</p>
+          <p className='text-slate-400'>Supercharge your maketing with a powerfull, easy-to-use platform built for results.</p>
         </div>
         <h1>Recent message</h1>
        </div>
