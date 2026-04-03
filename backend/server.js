146ec781f59f0722a327fc5b76d88eb89a73d13e
Bullet2004 <buithecuong2004@gmail.com>
2026-04-03 15:44:56 +0700

    fix: resolve server error

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,10 @@ import {serve} from 'inngest/express'
 import { clerkMiddleware } from '@clerk/express'
 import userRouter from './routes/userRoutes.js';
 import postRouter from './routes/postRoute.js';
-import { setupSocket } from './configs/socket.js';
-import http from 'http';
 import storyRouter from './routes/storyRoute.js';
 import messageRouter from './routes/messageRoute.js';
 
 const app = express();
-const server = http.createServer(app);
-const io = setupSocket(server);
 
 await connectDB();
 
@@ -22,7 +18,7 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Server is running')
 })
 app.use('/api/inngest', serve({ client: inngest, functions }))
@@ -31,10 +27,4 @@ app.use('/api/post', postRouter)
 app.use('/api/story', storyRouter)
 app.use('/api/message', messageRouter)
 
-const PORT = process.env.PORT || 4000;
-
-server.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
-
-export { io };
+export default app; 
